c0878b54ea3b059ccdeb1e0a246394fceedced53
Layne <saxalayne@gmail.com>
2023-04-05 10:09:19 -0600

    added more to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,77 @@
 const imgLeft = document.querySelector("#image-card1 > img");
 const imgRight = document.querySelector("#image-card2 > img");
+
 const pickMeBtn1 = document.querySelector("#selector1");
 const pickMeBtn2 = document.querySelector("#selector2");
 
+const foodTitle1 = document.querySelector('#title-1');
+const foodTitle2 = document.querySelector('#title-2');
+
+let currentBreakfast = null;
 
 fetch("http://localhost:3000/breakfast")
   .then((r) => r.json())
   .then((breakfast) => {
-    console.log(breakfast);
+    imgLeft.src = breakfast[0].image;
+    imgRight.src = breakfast[1].image;
+
+    breakfastArray = breakfast;
+    });
     
     pickMeBtn1.addEventListener("click", () => {
-      imgRight.src = breakfast[2].image;
-    });
-    
+      let randomIndex = Math.floor(Math.random() * breakfastArray.length);
+      let newBreakfast = breakfastArray[randomIndex];
+
+      if (newBreakfast.id !== currentBreakfast?.id) {
+        imgRight.src = newBreakfast.image;
+        foodTitle2.textContent = newBreakfast.name;
+        currentBreakfast = newBreakfast;
+      }});
+
     pickMeBtn2.addEventListener("click", () => {
-      imgLeft.src = breakfast[2].image;
+      let randomIndex = Math.floor(Math.random() * breakfastArray.length);
+      let newBreakfast = breakfastArray[randomIndex];
+  
+      if (newBreakfast.id !== currentBreakfast?.id) {
+        imgLeft.src = newBreakfast.image;
+        foodTitle1.textContent = newBreakfast.name;
+        currentBreakfast = newBreakfast;
+        }
+
+    imgLeft.addEventListener("mouseover", () => {
+      console.log('hello');
     });
-      
+
   });
+    
+    // imgLeft.addEventListener("mouseover", () => {
+    //   console.log(breakfast.note);
+    // });
+  
 
-imgLeft.addEventListener("mouseover", () => {});
+// imgLeft.addEventListener("mouseover", () => {
+//   console.log(breakfast.note);
+// });
 
-imgRight.addEventListener("mouseover", () => {});
+// imgRight.addEventListener("mouseover", () => {});
 
 
 //form hidden until button click
-function showComments() {
-  const x = document.getElementById("commentsSection");
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
-}
+// function showComments() {
+//   const x = document.getElementById("commentsSection");
+//   if (x.style.display === "none") {
+//       x.style.display = "block";
+//   } else {
+//       x.style.display = "none";
+//   }
+// }
 
-function addComment() {
-  const comment = document.getElementById("commentBox").value;
-  const node = document.createElement("p");
-  const textnode = document.createTextNode(comment);
-  node.appendChild(textnode);
-  document.getElementById("commentList").appendChild(node);
-  document.getElementById("commentBox").value = "";
-}
+// function addComment() {
+//   const comment = document.getElementById("commentBox").value;
+//   const node = document.createElement("p");
+//   const textnode = document.createTextNode(comment);
+//   node.appendChild(textnode);
+//   document.getElementById("commentList").appendChild(node);
+//   document.getElementById("commentBox").value = "";
+// }
 
