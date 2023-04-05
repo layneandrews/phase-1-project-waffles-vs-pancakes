fbdb73d37a6cb541883fd39762a81a19cee481db
Layne <saxalayne@gmail.com>
2023-04-05 08:43:13 -0600

    functionality to pickMeBtn event listeners

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,27 @@
 const imgLeft = document.querySelector("#image-card1 > img");
 const imgRight = document.querySelector("#image-card2 > img");
+const pickMeBtn1 = document.querySelector("#selector1");
+const pickMeBtn2 = document.querySelector("#selector2");
+
 
 fetch("http://localhost:3000/breakfast")
   .then((r) => r.json())
   .then((breakfast) => {
     console.log(breakfast);
+    
+    pickMeBtn1.addEventListener("click", () => {
+      imgRight.src = breakfast[2].image;
+    });
+    
+    pickMeBtn2.addEventListener("click", () => {
+      imgLeft.src = breakfast[2].image;
+    });
+      
   });
 
 imgLeft.addEventListener("mouseover", () => {});
 
 imgRight.addEventListener("mouseover", () => {});
-//the 10 lines below this comment just select the 'pick me' buttons and set event listeners to them
-const pickMeBtn1 = document.querySelector(".selector1");
-const pickMeBtn2 = document.querySelector(".selector2");
-
-pickMeBtn1.addEventListener("click", () => {});
-
-pickMeBtn2.addEventListener("click", () => {});
-
 
 
 //form hidden until button click
